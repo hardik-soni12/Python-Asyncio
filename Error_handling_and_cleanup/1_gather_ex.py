7c97a27f5972aeafed8503aab5eca0e94274fcde
Hardik Soni <imailhr1k@gmail.com>
2026-07-08 11:37:44 +0530

    catching bad exception

--- Error_handling_and_cleanup/1_gather_ex.py
+++ Error_handling_and_cleanup/1_gather_ex.py
@@ -7,13 +7,30 @@ async def fetch_data(url):
     await asyncio.sleep(1)
     return "data"
 
+# async def main():
+#     results = await asyncio.gather(
+#         fetch_data("good.com"),
+#         fetch_data("bad.com"),  # ← This fails
+#         fetch_data("good.com"),
+#     )
+#     print(results)  # Never reaches here
+
 async def main():
     results = await asyncio.gather(
         fetch_data("good.com"),
-        fetch_data("bad.com"),  # ← This fails
+        fetch_data("bad.com"),
         fetch_data("good.com"),
+        return_exceptions=True  # ← This line return_exceptions=True = "Give me errors as values, not crashes"
     )
-    print(results)  # Never reaches here
+    
+    # results = ['data', ValueError(...), 'data']
+    # No crash. Exceptions are in the list.
+    
+    for result in results:
+        if isinstance(result, Exception):
+            print(f"Error: {result}")
+        else:
+            print(f"Success: {result}")
 
 
 asyncio.run(main())
